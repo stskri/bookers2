e6489f8ef33986e5e438ee970e4b024e4a2721b6
stskri <177446189+stskri@users.noreply.github.com>
2024-09-02 16:03:47 +0000

    [finish]users_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.jind(params[:id])
+    @user.update
+    redirect_to user_path(user.id)
+  end
+  
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root to: 'homes#top', as: "top"
   get "/homes/about" => "homes#about", as: "about"
   resources :books, only: [:new, :create, :index, :edit, :show, :destroy]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
