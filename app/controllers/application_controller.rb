4fb16e00b628e9728e6fcf3134a00e924e85b337
stskri <177446189+stskri@users.noreply.github.com>
2024-09-03 11:25:32 +0000

    [finish]login,signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     books_path
   end
-  
+
   def after_sign_out_path_for(resource)
     top_path
   end
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
 
 end
