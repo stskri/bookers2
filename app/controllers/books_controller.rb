7e2593fa7c800781109adf48c5ba9926d077b49b
stskri <177446189+stskri@users.noreply.github.com>
2024-09-03 06:23:39 +0000

    [finish]books_update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,12 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to books_path
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
 
 
   private
