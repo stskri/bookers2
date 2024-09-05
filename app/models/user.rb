1f8dda8db5ea4f5671fe2bf5f87061539a6628c5
stskri <177446189+stskri@users.noreply.github.com>
2024-09-05 12:00:34 +0000

    [finish]header

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2}
+  validates :name, presence: true,length: { minimum: 2, maximum: 20}
+  validates :introdction, length: { maximum: 50}
 
   has_many :books, dependent: :destroy
 
